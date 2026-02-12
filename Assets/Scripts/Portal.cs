a32f9ea0029f6d71073d8652032f7aedd886a1c8
suhyzxn <kimsuhyeon360@gmail.com>
2026-02-12 10:48:52 +0900

    feat: change portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,7 +5,7 @@ public class Portal : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && ScoreManager.instance.score >= 3)
         {
             SceneManager.LoadScene("2D");
         }
